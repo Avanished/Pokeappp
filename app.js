f4d6c61db07bb3dd437526990e0229cc61c8c694
Diego Chávez Martínez <diego.chavez@safi.unam.mx>
2024-05-07 16:59:31 -0600

    finalizacion

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const namePoke2 = document.querySelector('#nombrePoke-rival');
 const poke2Tipo = document.querySelector('#tipoRival');
 const poke2Ataque = document.querySelector('#ataqueRival');
 
+const imgPoke = document.querySelector('#poke');
+const namePoke = document.querySelector('#nombrePoke-propio');
+const pokeTipo = document.querySelector('#tipoPropio');
+const pokeAtaque = document.querySelector('#ataquePropio');
+
+const input = document.querySelector('#input');
+const btnElegir = document.querySelector('#btn-poke');
+const btnPelear  = document.querySelector('#combate');
 
 const getNumRandom = () => {
     let min = Math.ceil(0);
@@ -12,7 +20,18 @@ const getNumRandom = () => {
   }
 
 const obtenerPokePropio = ()=>{
+    const num = input.value;
 
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{
+        console.log(res);
+        return res.data
+    }).then((res)=>{
+        console.log(res);
+        imgPoke.src = res.sprites.back_default;
+        pokeTipo.innerHTML = res.types[0].type.name;
+        namePoke.innerHTML = res.name;
+        pokeAtaque.innerHTML = res.stats[0].base_stat;
+    })
 }
 
 const obtenerPokeRival = () =>{
@@ -20,7 +39,7 @@ const obtenerPokeRival = () =>{
     const numPokeRival = getNumRandom();
 
     axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{
-
+        console.log(res);
         return res.data
     }).then((res)=>{
         console.log(res);
@@ -29,13 +48,20 @@ const obtenerPokeRival = () =>{
         namePoke2.innerHTML = res.name;
         poke2Ataque.innerHTML = res.stats[0].base_stat;
     })
+
+    console.log(imgPoke2);
 }
 
 const combate = ()=>{
     
+    const ataqueRival = parseInt(poke2Ataque.textContent);
+    const ataquePropio = parseInt(pokeAtaque.textContent);
+
+    console.log(ataquePropio);
+    
 }
 
-window.addEventListener('load', obtenerPokeRival)
-
-
+window.addEventListener('load', obtenerPokeRival);
+btnElegir.addEventListener('click', obtenerPokePropio);
+btnPelear.addEventListener('click', combate);
 
