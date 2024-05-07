e0115402b1d8f2cb87ea2506be7d936f90dd5598
DiegoChávez <diego.chav.mart@gmail.com>
2024-05-06 23:41:29 -0600

    Planteamiento del ejercicio

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const poke = document.querySelector('#poke');
-const pokeStats = document.querySelector('#tipoRival');
+const imgPoke = document.querySelector('#poke');
+const namePoke = document.querySelector('#nombrePoke-propio');
+const pokeStats = document.querySelector('#tipoPropio');
 
-let poke2 = document.querySelector('#poke2');
-const poke2Stats = document.querySelector('#tipoPropio');
+const imgPoke2 = document.querySelector('#poke2');
+const namePoke2 = document.querySelector('#nombrePoke-rival');
+const poke2Stats = document.querySelector('#tipoRival');
 
 const btn = document.querySelector('#btn-eleccion');
+const input = document.querySelector('#numPoke');
 
-const input = document.querySelector('#numPoke')
+const getNumRandom = () => {
+    let min = Math.ceil(0);
+    let max = Math.floor(1001);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
 btn.addEventListener('click',()=>{
 
@@ -19,20 +26,25 @@ btn.addEventListener('click',()=>{
     }).then((res)=>{
 
         console.log(res.sprites.back_default);
-        poke.src = res.sprites.back_default;
+        imgPoke.src = res.sprites.back_default;
         pokeStats.innerHTML = res.types[0].type.name;
+        namePoke.innerHTML = res.name;
     })
 
 });
 
 window.addEventListener('load',()=>{
-    axios.get('https://pokeapi.co/api/v2/pokemon/4/').then((res)=>{
+
+    const numPokeRival = getNumRandom();
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{
 
         return res.data
     }).then((res)=>{
         console.log(res);
-        poke2.src = res.sprites.front_default;
+        imgPoke2.src = res.sprites.front_default;
         poke2Stats.innerHTML = res.types[0].type.name;
+        namePoke2.innerHTML = res.name;
     })
 })
 
